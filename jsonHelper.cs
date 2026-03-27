7a747db352a218aab11f567218bf4cd6069eb981
PanTerra4464 | Michael Wilder <gamedevpntrr@gmail.com>
2026-03-27 19:49:55 +0800

    Update jsonHelper.cs

--- jsonHelper.cs
+++ jsonHelper.cs
@@ -4,8 +4,59 @@ internal class JsonHelper
 {
     static private string ProjectDir = null;
 
+
+    private bool ProjectDirNotSetError()
+    {
+        if (ProjectDir == null)
+        {
+            Console.WriteLine("Error: ProjectDir variable is not set");
+            return false;
+        }
+        return true;
+    }
+
+    private bool InvalidProjectDirError(string projectDir)
+    {
+        if (!Directory.Exists(projectDir))
+        {
+            Console.WriteLine("Error: ProjectDir is specified as an empty path");
+            return false;
+        }
+        if (string.IsNullOrEmpty(projectDir))
+        {
+            Console.WriteLine("Error: ProjectDir directory does not exist");
+            return false;
+        }
+        return true;
+    }
+
+    private bool FileAccessError(string fileName)
+    {
+        if (System.IO.File.Exists(fileName))
+        {
+            Console.WriteLine("Error: cannot create or delete file — check if it exists");
+            return false;
+        }
+        return true;
+    }
+    private bool FileAccessError(string[] fileName)
+    {
+        foreach (string file in fileName)
+        {
+            if (System.IO.File.Exists(file))
+            {
+                Console.WriteLine("Error: cannot create or delete file — check if it exists");
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     public void SetRootPath(string projectDir)
     {
+        if (InvalidProjectDirError(projectDir)) return;
+
         ProjectDir = projectDir;
     }
 
@@ -16,16 +67,25 @@ internal class JsonHelper
 
     public void CreateFile(string fileName)
     {
+        if (ProjectDirNotSetError()) return;
+        if (FileAccessError(fileName)) return;
+
         using (File.Create($@"{ProjectDir}\{fileName}.json")) { }
     }
 
     public void CreateFile(string filePath, string fileName)
     {
+        if (FileAccessError(fileName)) return;
+        if (InvalidProjectDirError(filePath)) return;
+
         using (File.Create($@"{filePath}\{fileName}.json")) { }
     }
 
     public void CreateFile(string[] fileName)
     {
+        if (ProjectDirNotSetError()) return;
+        if (FileAccessError(fileName)) return;
+
         for (int i = 0; i < fileName.Length; i++)
         {
             CreateFile(fileName[i]);
@@ -34,7 +94,10 @@ internal class JsonHelper
 
     public void CreateFile(string filePath, string[] fileName)
     {
-        for (int i = 0;i < fileName.Length; i++)
+        if (FileAccessError(fileName)) return;
+        if (InvalidProjectDirError(filePath)) return;
+
+        for (int i = 0; i < fileName.Length; i++)
         {
             CreateFile(filePath, fileName[i]);
         }
@@ -42,16 +105,25 @@ internal class JsonHelper
 
     public void DeleteFile(string fileName)
     {
+        if (ProjectDirNotSetError()) return;
+        if (FileAccessError(fileName)) return;
+
         File.Delete($@"{ProjectDir}\{fileName}.json");
     }
 
     public void DeleteFile(string filePath, string fileName)
     {
+        if (FileAccessError(fileName)) return;
+        if (InvalidProjectDirError(filePath)) return;
+
         File.Delete($@"{filePath}\{fileName}.json");
     }
 
     public void DeleteFile(string[] fileName)
     {
+        if (ProjectDirNotSetError()) return;
+        if (FileAccessError(fileName)) return;
+
         for (int i = 0; i < fileName.Length; i++)
         {
             DeleteFile(fileName[i]);
@@ -60,6 +132,9 @@ internal class JsonHelper
 
     public void DeleteFile(string filePath, string[] fileName)
     {
+        if (FileAccessError(fileName)) return;
+        if (InvalidProjectDirError(filePath)) return;
+
         for (int i = 0; i < fileName.Length; i++)
         {
             DeleteFile(filePath, fileName[i]);
@@ -68,18 +143,24 @@ internal class JsonHelper
 
     public void SaveFile(string fileName, object data)
     {
+        if (ProjectDirNotSetError()) return;
+
         string jsonString = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
         File.WriteAllText($@"{ProjectDir}\{fileName}.json", jsonString);
     }
 
     public void SaveFile(string filePath, string fileName, object data)
     {
+        if (InvalidProjectDirError(filePath)) return;
+
         string jsonString = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
         File.WriteAllText($@"{filePath}\{fileName}.json", jsonString);
     }
 
     public void SaveFile(string[] fileName, object[] data)
     {
+        if (ProjectDirNotSetError()) return;
+
         for (int i = 0; i < fileName.Length; i++)
         {
             SaveFile(fileName[i], data[i]);
@@ -88,6 +169,8 @@ internal class JsonHelper
 
     public void SaveFile(string filePath, string[] fileName, object[] data)
     {
+        if (InvalidProjectDirError(filePath)) return;
+
         for (int i = 0; i < fileName.Length; i++)
         {
             SaveFile(filePath, fileName[i], data[i]);
@@ -96,9 +179,9 @@ internal class JsonHelper
 
     public T LoadFile<T>(out T newVariable, string fileName)
     {
-            string jsonString = File.ReadAllText($@"{ProjectDir}\{fileName}.json");
-            newVariable = JsonSerializer.Deserialize<T>(jsonString);
-            return newVariable;
+        string jsonString = File.ReadAllText($@"{ProjectDir}\{fileName}.json");
+        newVariable = JsonSerializer.Deserialize<T>(jsonString);
+        return newVariable;
     }
 
     public T LoadFile<T>(out T newVariable, string filePath, string fileName)
